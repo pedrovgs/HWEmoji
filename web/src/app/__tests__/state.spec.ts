09d234553c80df82bdbb20aebe206c66996f8458
Pedro Gomez <pedro@goodnotesapp.com>
2023-04-23 16:06:49 +0200

    Format code

--- web/src/app/__tests__/state.spec.ts
+++ web/src/app/__tests__/state.spec.ts
@@ -3,27 +3,25 @@ import { defaultAppState, selectEmoji, selectMode } from "../domain/state";
 import { gemoji } from "gemoji";
 
 describe("State management", () => {
+  it("Should use grinning emoji by default", () => {
+    const expectedEmoji = gemoji[0];
+    expect(defaultAppState.selectedEmoji).toEqual(expectedEmoji);
+    expect(expectedEmoji.emoji).toEqual("😀");
+  });
 
-    it("Should use grinning emoji by default", () => {
-        const expectedEmoji = gemoji[0];
-        expect(defaultAppState.selectedEmoji).toEqual(expectedEmoji);
-        expect(expectedEmoji.emoji).toEqual("😀");
-    })
+  it("Should use collect mode by default", () => {
+    expect(defaultAppState.selectedMode).toEqual(WorkMode.collectData);
+  });
 
-    it("Should use collect mode by default", () => {
-        expect(defaultAppState.selectedMode).toEqual(WorkMode.collectData);
-    })
+  it("Should update state and use the new selected emoji", () => {
+    const emojiToSelect = gemoji[1];
+    const state = selectEmoji(defaultAppState, emojiToSelect);
+    expect(state.selectedEmoji).toEqual(emojiToSelect);
+  });
 
-    it("Should update state and use the new selected emoji", () => {
-        const emojiToSelect = gemoji[1]
-        const state = selectEmoji(defaultAppState, emojiToSelect);
-        expect(state.selectedEmoji).toEqual(emojiToSelect);
-    })
-
-    it("Should update mode and use the new selected mode", () => {
-        const modeToSelect = WorkMode.test;
-        const state = selectMode(defaultAppState, modeToSelect);
-        expect(state.selectedMode).toEqual(modeToSelect);
-    })
-
+  it("Should update mode and use the new selected mode", () => {
+    const modeToSelect = WorkMode.test;
+    const state = selectMode(defaultAppState, modeToSelect);
+    expect(state.selectedMode).toEqual(modeToSelect);
+  });
 });
