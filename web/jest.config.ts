bc6407aa0594229200079c33519e4cfe6c0bdd50
Pedro Gomez <pedro@goodnotesapp.com>
2023-04-23 16:06:39 +0200

    Get tests passing

--- web/jest.config.ts
+++ web/jest.config.ts
@@ -1,13 +1,7 @@
 import type { Config } from "@jest/types";
 
 const config: Config.InitialOptions = {
-  globals: {
-    "ts-jest": {
-      tsconfig: "tsconfig.json",
-      diagnostics: true,
-    },
-    NODE_ENV: "test",
-  },
+  preset: "ts-jest",
   setupFiles: ["jest-canvas-mock"],
   moduleDirectories: ["node_modules", "src"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"],
@@ -19,8 +13,10 @@ const config: Config.InitialOptions = {
   },
   transform: {
     "^.+\\.ts$": "ts-jest",
+    "^.+\\.js$": "ts-jest",
     "^.+\\.tsx$": "ts-jest",
   },
+  transformIgnorePatterns: ["node_modules/(?!gemoji)"],
   globalSetup: "./jest/global-setup.ts",
   verbose: true,
   testEnvironment: "jsdom",
